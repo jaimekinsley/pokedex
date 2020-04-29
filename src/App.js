29c07bda23e7d920e0e5b8f99a53d607830e280a
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-29 15:21:46 -0700

    got the searchbar event handler working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  handleChange = (event) => {
+    // gets the value of input
+    const value = event.target.value;
+    this.setState({ searchQuery: value });
+    console.log(value)
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange}/>
+        <button>Search</button>
+      </div>
+    )
+  }
 }
-
-export default App;
