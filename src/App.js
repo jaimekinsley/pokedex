7902854ac23dd297fb57be351bce4ed74743143e
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-29 21:39:55 -0700

    gets attack search to work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
 import request from 'superagent';
 
-// const data = [
-//   {
-//     character: 'some character 1',
-//     type: 'some type 1'
-//   },
-//   {
-//     character: 'some character 2',
-//     type: 'some type 2'
-//   },
-//   {
-//     character: 'some character 3',
-//     type: 'some type 3'
-//   }
-// ]
-
-
 
 export default class App extends Component {
 state = {
@@ -32,23 +16,46 @@ state = {
 
   handleClick = async () => {
     const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`);
-    console.log(fetchedData)
-    this.setState({ data: fetchedData.body.results })
+    this.setState({ data: fetchedData.body.results });
+  }
+
+  handleAttackClick = async () => {
+    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex`);
+    const attackData = fetchedData.body.results.filter((e) => {
+      if (e.attack >= this.state.searchQuery){
+      return true;
+      }
+      return false;
+    });
+    this.setState({ data: attackData });
   }
 
   
 
   render() {
     return (
-      <div>
+      <div> 
+
+        <div className='searchName'>
+        Search by name or type:
        <input onChange={this.handleChange}/>
        <button onClick={this.handleClick}>Search</button>
-       <div>
+       </div>
+
+      <div className='searchAttack'>
+        Search by attack minimum:
+      <input onChange={this.handleChange}/>
+       <button onClick={this.handleAttackClick}>Search</button>
+      </div>
+
+
+
+             <div>
              {
                  this.state.data.map(data => {
                  return <>
                  <h2>{data.pokemon}</h2>
-                 <img src={data.url_image}/>
+                 <img src={data.url_image} alt={data.pokemon}/>
                  <h3>Type: {data.type_1}, {data.type_2}</h3>
                  <h3>Attack: {data.attack}</h3>
                  <h3>Defense: {data.defense}</h3>
